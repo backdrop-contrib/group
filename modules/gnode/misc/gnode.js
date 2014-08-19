1eed97f22501f155997527955041ca38c0149cf8
Kristiaan Van den Eynde <magentix@gmail.com>
2014-08-19 10:53:52 +0200

    Issue #2317111 by kristiaanvandeneynde: Fixed Group perm vs site perm.

--- modules/gnode/misc/gnode.js
+++ modules/gnode/misc/gnode.js
@@ -3,10 +3,12 @@
 Drupal.behaviors.groupFieldsetSummaries = {
   attach: function (context) {
     $('fieldset.node-form-group-information', context).drupalSetSummary(function (context) {
-      var input = $('#edit-group-settings-group', context).val();
+      var input = $('#edit-group-settings-group', context),
+          select = $('#edit-group-settings-gid', context),
+          value = input.length != 0 ? input.val() : select.val();
 
-      if (input != '') {
-        return $.trim(input);
+      if (value != '') {
+        return $.trim(input.length != 0 ? value : select.children(':selected').text());
       }
 
       return Drupal.t('Not a group node');
